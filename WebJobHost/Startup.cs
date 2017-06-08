50bc549890e958f6b38ad8b156d8ced90bb7b794
MDSEva <seteam@outlook.kr>
2017-06-08 16:09:45 +0900

    sencond

--- WebJobHost/Startup.cs
+++ WebJobHost/Startup.cs
@@ -9,6 +9,7 @@ namespace WebJobHost
     {
         public void Configuration(IAppBuilder app)
         {
+            // 
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
             // Do nothing on startup
         }
